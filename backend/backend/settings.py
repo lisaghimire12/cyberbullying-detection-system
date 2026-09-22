f52cebbe8fae5670eb1eae7a20641e488eb39c8c
lisaghimire12 <lisa.ghimire1@gmail.com>
2026-09-22 22:02:58 +0530

    Configure Django for production environment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,9 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get(
+    "ALLOWED_HOSTS",
+    "localhost,127.0.0.1"
+).split(",")
 
 
 # Application definition
